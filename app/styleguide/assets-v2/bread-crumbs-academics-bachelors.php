fd009320e3c2eab7f0eb0154cca535623cf64fc5
Jake Salazar <jake.salazar@gmail.com>
2021-09-08 09:44:29 -0500

    more clean up of old files

--- app/styleguide/assets-v2/bread-crumbs-academics-bachelors.php
+++
@@ -1,8 +0,0 @@
-<div class="main-body-crumbs">
-    <a href="/academics/">Academics</a>
-    <span class="glyphicon glyphicon-menu-right" aria-hidden="true"></span>
-
-    <a href="/academics/bachelors_degree_programs/">Bachelor's</a>
-    <span class="glyphicon glyphicon-menu-right" aria-hidden="true"></span>
-
-</div>
--- app/styleguide/assets-v2/soe-main-nav-none.php
+++
@@ -1,5 +0,0 @@
-<!-- this template name was changed from soe-main-event -->
-    <main class="soedu-main-content">
-        <!-- SOE MAIN BODY -->
-        <?php include $HTML->pageBody; ?>
-    </main>
